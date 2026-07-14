71a4dc1da2c8bfbe004b63e3d3ca0b8a81b7a9f1
iolindt <ramazzanov@gmail.com>
2026-07-14 17:09:53 +0300

    Create weather.py

---
+++ weather.py
@@ -0,0 +1,14 @@
+class Weather:
+
+    def __init__(
+        self,
+        city,
+        temperature,
+        humidity,
+        condition
+    ):
+
+        self.city = city
+        self.temperature = temperature
+        self.humidity = humidity
+        self.condition = condition
